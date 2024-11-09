8d1d84d752e1f69b3eb7dc9419b42d7a0ba4fabe
rbhavsar3 <rbhavsar3@slb.com>
2024-11-09 20:24:33 +0530

    added gtm script

--- src/app/google-analytics.servive.ts
+++ src/app/google-analytics.servive.ts
@@ -10,23 +10,37 @@ export class GoogleAnalyticsService {
     this.renderer = rendererFactory.createRenderer(null, null);
   }
  
-  loadGoogleAnalytics() {
+  loadTrackingScripts() {
+    // Load Google Analytics
     const gtagScript = this.renderer.createElement('script');
-    this.renderer.setAttribute(gtagScript, 'src', `https://www.googletagmanager.com/gtag/js?id=G-27603PZED1`);
+    this.renderer.setAttribute(gtagScript, 'src', 'https://www.googletagmanager.com/gtag/js?id=G-27603PZED1');
     this.renderer.setAttribute(gtagScript, 'async', '');
     const head = this.renderer.selectRootElement('head', true);
     this.renderer.appendChild(head, gtagScript);
- 
-    const script = this.renderer.createElement('script');
-    const scriptContent = `
+  
+    const gaScript = this.renderer.createElement('script');
+    const gaScriptContent = `
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
       gtag('config', 'G-27603PZED1');
     `;
-    this.renderer.setProperty(script, 'textContent', scriptContent);
-    this.renderer.appendChild(head, script);
+    this.renderer.setProperty(gaScript, 'textContent', gaScriptContent);
+    this.renderer.appendChild(head, gaScript);
+  
+    // Load Google Tag Manager
+    const gtmScript = this.renderer.createElement('script');
+    const gtmScriptContent = `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-KPPFX5CZ');
+    `;
+    this.renderer.setProperty(gtmScript, 'textContent', gtmScriptContent);
+    this.renderer.appendChild(head, gtmScript);
   }
+  
  
   trackEvent(eventName: string, eventCategory: string) {
       gtag('event', eventName, {
