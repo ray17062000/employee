c531426712264acfee5f7f689e6337f817e4c357
rbhavsar3 <rbhavsar3@slb.com>
2024-11-11 12:10:12 +0530

    email_domain changes

--- src/app/dataLayaer.service.ts
+++ src/app/dataLayaer.service.ts
@@ -29,7 +29,7 @@ export class DataLayerService {
   }
 
   logCustomDimensionTest(name: string, value: string) {
-    const hit = {email : value};
+    const hit = {email_domain : value};
     this.pingHome(hit);
   }
 }
