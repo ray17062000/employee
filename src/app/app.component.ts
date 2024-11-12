ff0905f2b5bf9dd928c4fb47beaf127b1254c04e
rbhavsar3 <rbhavsar3@slb.com>
2024-11-12 13:46:33 +0530

    using gtm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent  {
   ngOnInit()
   {    
     this.googleAnalyticsService.loadGoogleAnalytics();
-    this.googleAnalyticsService.setDomainProperties();
+    this.dataLayerService.logCustomDimensionTest("email_domain", "nihaan2");
   } 
 
   gotoHome()
