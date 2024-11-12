d97b53acac804b9e8e57ec37ee4619edefb8dc8b
rbhavsar3 <rbhavsar3@slb.com>
2024-11-12 10:49:28 +0530

    email domain to riya5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent  {
   ngOnInit()
   {    
     this.googleAnalyticsService.loadGoogleAnalytics();
-    this.dataLayerService.logCustomDimensionTest('email_domain', 'riya4');
+    this.dataLayerService.logCustomDimensionTest('email_domain', 'riya5');
   } 
 
   gotoHome()
