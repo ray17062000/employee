630750eb6f6f260772b62f93c9d59786a6c9c54f
rbhavsar3 <rbhavsar3@slb.com>
2024-11-13 11:44:15 +0530

    removed user property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,8 +18,7 @@ export class AppComponent  {
   ngOnInit()
   {    
     this.googleAnalyticsService.loadGoogleAnalytics();
-    // this.dataLayerService.logCustomDimensionTest("email_domain", "with_gtm");
-    this.googleAnalyticsService.setDomainProperties();
+
   } 
 
   gotoHome()
